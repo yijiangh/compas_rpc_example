0fb90b5f21e88759e2523df70ff63aa73dc5a18f
Yijiang Huang <yijiangh@mit.edu>
2020-05-08 08:38:56 -0400

    rpc example added

--- src/compas_rpc_example/__init__.py
+++ src/compas_rpc_example/__init__.py
@@ -3,6 +3,9 @@ from __future__ import print_function
 from .__version__ import __author__, __author_email__, __copyright__, \
                         __description__, __license__, __title__, __url__, __version__
 
+# from .interlock import *
+from .test_demo import *
+
 __all__ = [
     '__author__', '__author_email__', '__copyright__', '__description__', 
     '__license__', '__title__', '__url__', '__version__', 
